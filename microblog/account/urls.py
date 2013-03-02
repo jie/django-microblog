0338b6304b69db4b8ed23fc6310cbc5f5e244c67
ZhouYang <zhouyang.me@gmail.com>
2013-03-02 14:27:42 +0800

    start a new project

--- microblog/account/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from account.views import *
-urlpatterns = patterns('',
-  (r'^signup/$',  signup_view),
-  (r'^login/$', login_view),
-  (r'^logout/$', logout_view),
-)
--- microblog/mail/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from mail.views import *
-urlpatterns = patterns('',
-  (r'^mailto/(\d+)/$', mail_view),
-  (r'^inbox/$', inbox_view),
-  (r'^sendbox/$', sendbox_view),
-  (r'^delete/(\d+)/', delete_mail),
-)
-
